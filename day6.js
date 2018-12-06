d2371feed5682c42a365f15d67a068fb2c9a7640
Ryan Zaki <ryan@zaki.io>
2018-12-06 13:20:46 -0500

    day 6 closest points

---
+++ day6.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+let lines = fs.readFileSync('day6_input.txt')
+		.toString()
+		.split('\r\n');
+lines.pop(); // account for the newline char at end of input
+
+const grid = {}
+
+manhattanDistance = function(p, q) {
+    return Math.abs(p.x - q.x) + Math.abs(p.y - q.y)
+}
+
+
+findClosestPoint = function(thisPoint, points) {
+    let leastDistance = Number.MAX_SAFE_INTEGER;
+    let closestPoint;
+    for(let i = 0; i < points.length; i++) {
+        if(thisPoint.id != points[i].id) {
+            const distance = manhattanDistance(thisPoint, points[i])
+            if (distance < leastDistance) {
+                leastDistance = distance;
+                closestPoint = points[i].id;
+            }
+        }
+    }
+    return closestPoint;
+}
+
+makeRectange = function (pointA, pointB) {
+    
+}
+
+let points = lines.map(line => {
+    return {
+        id: line.split(" ")[0] + line.split(" ")[1],
+        x:  line.split(", ")[0],
+        y:  line.split(", ")[1],
+    }}
+);
+for(let i = 0; i < points.length; i++) {
+    const closestPoint = findClosestPoint(points[i], points)
+    console.log(`closest point to ${points[i].id} is ${closestPoint}`);
+}
+
+
+
+// console.log(lines);
+// console.log(manhattanDistance(points[1], points[2]))
+
+
